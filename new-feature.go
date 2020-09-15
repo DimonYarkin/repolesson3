a8b19058cfe1f3bfcb914f97b1f43337de6b4521
DimonYarkin <51164050+DimonYarkin@users.noreply.github.com>
2020-09-15 12:57:02 +0300

    Update new-feature.go

--- new-feature.go
+++ new-feature.go
@@ -6,17 +6,16 @@
 Решение
 
 def my_div(arg1, arg2):
-    возврат arg1 / arg2
+    return arg1 / arg2
 
 
-пока это правда:
-    попробуй:
+while True:
+    try:
         arg1 = int(input("Введите делимое целое число"))
         arg2 = int(input("Введите делитель целое число"))
         print(f"Результат деления ({arg1} на {arg2}) = {my_div(arg1, arg2):.2}")
-        перерыв
-    кроме ZeroDivisionError:
+        break
+    except ZeroDivisionError:
         print("Делитель равен нулю, на 0 делить нельзя. Повторите ввод")
-    кроме ValueError:
+    except ValueError:
         print("Один из аргументов не число повторите ввод")
-
